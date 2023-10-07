a5fb63da49a9c1647431a2ca6738f01fe817839a
Pieter Svenson <pietelite@gmail.com>
2023-10-07 02:22:17 -0500

    enable google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,9 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-HPCT9XQWT3',
+        },
       }),
     ],
   ],
